b1c94776905a208ffc806fbb048047251f9e2d1e
Isadora Martins <isadoramartins1906@gmail.com>
2025-04-25 22:44:26 -0300

    Add files via upload

---
+++ entradas.py
@@ -0,0 +1,12 @@
+# fruta = input("Digite uma fruta de sua escolha: ")
+# print(fruta)
+
+codigo = int(input("Digite o codigo do funcionario: "))
+nome = input("Digite o nome do funcionario: ")
+salario = float(input("Informe o salario: "))
+ativo = True
+
+print ("Codigo: %d"%codigo)
+print ("Nome: %s"%nome)
+print ("Salario: %.2f"%salario)
+print ("Ativo: %r"%ativo)
---
+++ tipo_variavel.py
@@ -0,0 +1,7 @@
+codigo = 10
+salario = 1500.00
+nome = "Jose"
+situacao = True
+
+print("Codigo: " + str(codigo) + "Nome: "+ nome + "O salario atual e de: " + str (salario))
+# ou print("Codigo:", codigo, "Nome: ",nome, "O salario atual e de: ",salario)
